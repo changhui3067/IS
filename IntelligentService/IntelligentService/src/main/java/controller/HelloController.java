74a1e04f45a6c232dd43e5cdf1cdabd591e73e24
Wu <I309994@global.corp.sap>
2016-03-15 20:10:02 +0800

    gitignore

--- IntelligentService/IntelligentService/src/main/java/controller/HelloController.java
+++ IntelligentService/IntelligentService/src/main/java/controller/HelloController.java
@@ -6,11 +6,16 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.ArrayList;
+
 @Controller
 @RequestMapping("/")
 public class HelloController {
 	@RequestMapping(method = RequestMethod.GET)
 	public String printWelcome() {
+		ArrayList<String> arr = new ArrayList<>();
 		return "hello";
+
+
 	}
 }
